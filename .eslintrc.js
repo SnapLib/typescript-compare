05dbeba4e019fd33342e4350ebc3bb4da2a6c614
Snap <XxSnapperGeexX@gmail.com>
2021-05-11 18:17:47 -0700

    Init

---
+++ .eslintrc.js
@@ -0,0 +1,39 @@
+"use strict";
+
+module.exports = {
+    env:
+    {
+        es2020: true,
+        node: true
+    },
+    extends:
+    [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended"
+    ],
+    parser: "@typescript-eslint/parser",
+    parserOptions:
+    {
+        ecmaVersion: 2020,
+        sourceType: "module"
+    },
+    plugins:
+    [
+        "@typescript-eslint"
+    ],
+    rules:
+    {
+        "@typescript-eslint/no-inferrable-types": "off",
+        "@typescript-eslint/no-empty-function": "warn",
+        "default-case": "error",
+        "default-case-last": "error",
+        "eol-last": "error",
+        "no-console": "warn",
+        "no-empty-function": "warn",
+        "no-implicit-coercion": "error",
+        "quotes": ["error", "double", {"avoidEscape": true, "allowTemplateLiterals": true}],
+        "no-tabs": "error",
+        "no-trailing-spaces": "error",
+        "semi": ["error", "always"]
+    }
+};
---
+++ .mocharc.js
@@ -0,0 +1,12 @@
+"use strict";
+
+const join = require("path").join
+
+module.exports =
+{
+    extension: ["spec.ts"],
+    recursive: true,
+    require: [join("ts-node", "register")],
+    spec: join("src", "test", "ts", "**", "*.spec.ts"),
+    ui: "tdd"
+}
