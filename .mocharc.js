2a1b424155d3deb93657265a8171f204c3db833a
Snap <XxSnapperGeexX@gmail.com>
2021-05-11 19:16:16 -0700

    Fix path to tests directory

--- .mocharc.js
+++ .mocharc.js
@@ -7,6 +7,6 @@ module.exports =
     extension: ["spec.ts"],
     recursive: true,
     require: [join("ts-node", "register")],
-    spec: join("src", "test", "ts", "**", "*.spec.ts"),
+    spec: join("src", "test", "**", "*.spec.ts"),
     ui: "tdd"
 }
