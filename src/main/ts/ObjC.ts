e71e1f186125a737cd36d66a1b01925239e5b64c
Snap <XxSnapperGeexX@gmail.com>
2021-05-13 01:15:10 -0700

    Have added keys use hasOwnProperty instead of "in"

--- src/main/ts/ObjC.ts
+++ src/main/ts/ObjC.ts
@@ -3,8 +3,8 @@ export class ObjectComparer<SourceType, TargetType>
     private readonly _srcObj: Readonly<SourceType>;
     private readonly _targetObj: Readonly<TargetType>;
     private readonly _omittedKeys: ReadonlyArray<string>;
-    private readonly _includedKeys: ReadonlyArray<string>;
     private readonly _addedKeys: ReadonlyArray<string>;
+    private readonly _includedKeys: ReadonlyArray<string>;
     // private readonly _alteredKeyValues: readonly Readonly<{key: string, originalValue: Readonly<unknown>, newValue: Readonly<unknown>}>[];
 
     public constructor(sourceObject: SourceType, targetObject: TargetType)
@@ -15,11 +15,11 @@ export class ObjectComparer<SourceType, TargetType>
         this._omittedKeys = Object.freeze(
             Object.keys(sourceObject).filter(srcObjKey => ! (srcObjKey in targetObject)));
 
+        this._addedKeys = Object.freeze(
+            Object.keys(targetObject).filter(targetObjKey => ! Object.prototype.hasOwnProperty.call(sourceObject, targetObjKey)));
+
         this._includedKeys = Object.freeze(
             Object.keys(sourceObject).filter(srcObjKey => srcObjKey in targetObject));
-
-        this._addedKeys = Object.freeze(
-            Object.keys(targetObject).filter(targetObjKey => ! (targetObjKey in sourceObject)));
     }
 
     public readonly get = Object.freeze({
@@ -29,19 +29,23 @@ export class ObjectComparer<SourceType, TargetType>
 
         omittedKeys: (): ReadonlyArray<string> => this._omittedKeys,
 
-        includedKeys: (): ReadonlyArray<string> => this._includedKeys,
+        addedKeys: (): ReadonlyArray<string> => this._addedKeys,
 
-        addedKeys: (): ReadonlyArray<string> => this._addedKeys
+        includedKeys: (): ReadonlyArray<string> => this._includedKeys
     });
 
     public readonly has = Object.freeze({
         omittedKeys: (): boolean => this._omittedKeys.length !== 0,
+
+        addedKeys: (): boolean => this._addedKeys.length !== 0,
 
         includedKeys: (): boolean => this._includedKeys.length !== 0
     });
 
     public readonly count = Object.freeze({
         omittedKeys: (): number => this._omittedKeys.length,
+
+        addedKeys: (): number => this._addedKeys.length,
 
         includedKeys: (): number => this._includedKeys.length
     });
