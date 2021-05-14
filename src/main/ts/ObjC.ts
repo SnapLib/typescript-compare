3055736e6956f01a8c225ddc7f6e85fdcccc85c3
Snap <XxSnapperGeexX@gmail.com>
2021-05-13 23:42:15 -0700

    Begin adding altered keys property

--- src/main/ts/ObjC.ts
+++ src/main/ts/ObjC.ts
@@ -5,12 +5,24 @@ export class ObjectComparer<SourceType, TargetType>
     private readonly _omittedKeys: ReadonlyArray<string>;
     private readonly _addedKeys: ReadonlyArray<string>;
     private readonly _includedKeys: ReadonlyArray<string>;
-    // private readonly _alteredKeyValues: readonly Readonly<{key: string, originalValue: Readonly<unknown>, newValue: Readonly<unknown>}>[];
+    private readonly _alteredKeyValues: readonly Readonly<{key: string, originalValue: Readonly<unknown>, newValue: Readonly<unknown>}>[];
 
     public constructor(sourceObject: SourceType, targetObject: TargetType)
     {
         this._srcObj = Object.freeze(sourceObject);
         this._targetObj = Object.freeze(targetObject);
+
+        const srcObjEntries: readonly Readonly<[string, unknown]>[] =
+            Object.freeze(Object.entries(sourceObject));
+
+        const targetObjEntries: readonly Readonly<[string, unknown]>[] =
+            Object.freeze(Object.entries(targetObject));
+
+        const srcObjKeys: ReadonlyArray<string> =
+            Object.freeze(Object.keys(sourceObject));
+
+        const targetObjKeys: ReadonlyArray<string> =
+            Object.freeze(Object.keys(targetObject));
 
         this._omittedKeys = Object.freeze(
             Object.keys(sourceObject).filter(srcObjKey => ! (srcObjKey in targetObject)));
@@ -20,6 +32,14 @@ export class ObjectComparer<SourceType, TargetType>
 
         this._includedKeys = Object.freeze(
             Object.keys(sourceObject).filter(srcObjKey => srcObjKey in targetObject));
+
+        this._alteredKeyValues = Object.freeze(
+            srcObjEntries.filter(srcObjEntry =>
+                targetObjEntries.some(targetObjEntry => srcObjEntry[0] === targetObjEntry[0] && srcObjEntry[1] !== targetObjEntry[1]))
+                .map(srcObjEntry => (Object.freeze({
+                    key: srcObjEntry[0],
+                    originalValue: Object.freeze(srcObjEntry[1]),
+                    newValue: Object.freeze(targetObjEntries.filter(e => e[0] === srcObjEntry[0])[0][1])}))));
     }
 
     public readonly get = Object.freeze({
@@ -31,7 +51,9 @@ export class ObjectComparer<SourceType, TargetType>
 
         addedKeys: (): ReadonlyArray<string> => this._addedKeys,
 
-        includedKeys: (): ReadonlyArray<string> => this._includedKeys
+        includedKeys: (): ReadonlyArray<string> => this._includedKeys,
+
+        alteredKeyValues: (): readonly Readonly<{key: string, originalValue: Readonly<unknown>, newValue: Readonly<unknown>}>[] => this._alteredKeyValues
     });
 
     public readonly has = Object.freeze({
