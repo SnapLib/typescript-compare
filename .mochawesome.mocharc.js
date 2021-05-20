dd623c9dc60dd6a1158264d3e4d1d1f0aa834de3
Snap <XxSnapperGeexX@gmail.com>
2021-05-19 23:12:04 -0700

    Add config options

--- .mochawesome.mocharc.js
+++ .mochawesome.mocharc.js
@@ -5,4 +5,15 @@ const mocharc = require("./.mocharc");
 module.exports =
 {
     ...mocharc,
+    reporter: "mochawesome",
+    reporterOptions: [
+        "autoOpen=true",
+        "consoleReporter=none",
+        "json=false",
+        "reportDir=./build/test-report",
+        "reportFilename=index",
+        "reportPageTitle=ObjectComparer test report",
+        "reportTitle=SnapLib Object Comparer Test Report",
+        "showSkipped=false"
+    ]
 }
