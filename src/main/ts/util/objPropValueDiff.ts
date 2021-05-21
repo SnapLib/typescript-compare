911194cba2aeac906e493855c4feb69f529d836c
Snap <XxSnapperGeexX@gmail.com>
2021-05-21 00:16:12 -0700

    Add type paremeters for source and target value type

--- src/main/ts/util/objPropValueDiff.ts
+++ src/main/ts/util/objPropValueDiff.ts
@@ -1,26 +1,26 @@
-export class ObjPropValueDiff
+export class ObjPropValueDiff<SourceValueType, TargetValueType>
 {
     private readonly _key: string;
-    private readonly _sourceValue: unknown;
-    private readonly _targetValue: unknown;
+    private readonly _sourceValue: Readonly<SourceValueType>;
+    private readonly _targetValue: Readonly<TargetValueType>;
 
     public readonly 0: string;
-    public readonly 1: unknown;
-    public readonly 2: unknown;
+    public readonly 1: Readonly<SourceValueType>;
+    public readonly 2: Readonly<TargetValueType>;
 
-    public constructor(key: string, sourceValue: unknown, targetValue: unknown)
+    public constructor(key: string, sourceValue: SourceValueType, targetValue: TargetValueType)
     {
-        this._key = key;
-        this[0] = key;
-        this._sourceValue = sourceValue;
-        this[1] = sourceValue;
-        this._targetValue = targetValue;
-        this[2] = targetValue;
+        this._key = Object.freeze(key);
+        this[0] = Object.freeze(key);
+        this._sourceValue = Object.freeze(sourceValue);
+        this[1] = Object.freeze(sourceValue);
+        this._targetValue = Object.freeze(targetValue);
+        this[2] = Object.freeze(targetValue);
     }
 
     public get key(): string { return this._key; }
-    public get sourceValue(): unknown { return this._sourceValue; }
-    public get targetValue(): unknown { return this._targetValue; }
+    public get sourceValue(): Readonly<SourceValueType> { return this._sourceValue; }
+    public get targetValue(): Readonly<TargetValueType> { return this._targetValue; }
 }
 
 export {ObjPropValueDiff as default};
