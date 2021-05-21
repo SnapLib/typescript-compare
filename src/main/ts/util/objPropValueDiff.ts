bb7870a0e15bf52c8b2e480586975de7aac619b9
Snap <XxSnapperGeexX@gmail.com>
2021-05-21 00:20:35 -0700

    Make source and target value unkown types readonly and freeze constructor arguments

--- src/main/ts/util/objPropValueDiff.ts
+++ src/main/ts/util/objPropValueDiff.ts
@@ -1,26 +1,26 @@
 export class ObjPropValueDiff
 {
     private readonly _key: string;
-    private readonly _sourceValue: unknown;
-    private readonly _targetValue: unknown;
+    private readonly _sourceValue: Readonly<unknown>;
+    private readonly _targetValue: Readonly<unknown>;
 
     public readonly 0: string;
-    public readonly 1: unknown;
-    public readonly 2: unknown;
+    public readonly 1: Readonly<unknown>;
+    public readonly 2: Readonly<unknown>;
 
     public constructor(key: string, sourceValue: unknown, targetValue: unknown)
     {
-        this._key = key;
-        this[0] = key;
-        this._sourceValue = sourceValue;
-        this[1] = sourceValue;
-        this._targetValue = targetValue;
-        this[2] = targetValue;
+        this._key = Object.freeze(key);
+        this[0] = Object.freeze(key);
+        this._sourceValue = Object.freeze(sourceValue);
+        this[1] = Object.freeze(sourceValue);
+        this._targetValue = Object.freeze(targetValue);
+        this[2] = Object.freeze(targetValue);
     }
 
     public get key(): string { return this._key; }
-    public get sourceValue(): unknown { return this._sourceValue; }
-    public get targetValue(): unknown { return this._targetValue; }
+    public get sourceValue(): Readonly<unknown> { return this._sourceValue; }
+    public get targetValue(): Readonly<unknown> { return this._targetValue; }
 }
 
 export {ObjPropValueDiff as default};
