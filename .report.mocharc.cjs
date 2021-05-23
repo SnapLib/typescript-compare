48f586fedd3482d0f49f860cf491cb6c443f71d3
Snap <XxSnapperGeexX@gmail.com>
2021-05-23 00:26:22 -0700

    Write out imported mocharc as fully qualified relative path

--- .report.mocharc.cjs
+++ .report.mocharc.cjs
@@ -4,13 +4,13 @@ const join = require("path").join;
 
 module.exports =
 {
-    ...require(".mocharc"),
+    ...require("./.mocharc.cjs"),
     reporter: "mochawesome",
     reporterOptions: [
         "autoOpen=true",
         "consoleReporter=none",
         "json=false",
-        `reportDir=${join(__dirname, "../../", "build", "test-report")}`,
+        `reportDir=${join(__dirname, "build", "test-report")}`,
         "reportFilename=index",
         "reportPageTitle=ObjectCompare test report",
         "reportTitle=SnapLib Object Comparer Test Report",
