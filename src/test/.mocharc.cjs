05315f4c8bb838020424ccfde25cee3b363782ec
Snap <XxSnapperGeexX@gmail.com>
2021-05-23 00:12:41 -0700

    Add cjs extension to js files

--- src/test/.mocharc.cjs
+++ src/test/.mocharc.cjs
@@ -1,12 +1,12 @@
 "use strict";
 
-const join = require("path").join
+const join = require("path").join;
 
 module.exports =
 {
     extension: ["spec.ts"],
     recursive: true,
     require: [join("ts-node", "register")],
-    spec: join("src", "test", "**", "*.spec.ts"),
+    spec: join(__dirname, "ts", "**", "*.spec.ts"),
     ui: "tdd"
 }
