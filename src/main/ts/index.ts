36754db56f22b8b258671a4054a8784a551a5f8d
Snap <XxSnapperGeexX@gmail.com>
2021-05-22 17:41:40 -0700

    Init with re-exports of all classes, interfaces, and functions

---
+++ src/main/ts/index.ts
@@ -0,0 +1,5 @@
+export {ObjectComparerCountQuery} from "./lib/objComparerQueries/objectComparerCountQuery";
+export {ObjectComparerHasQuery} from "./lib/objComparerQueries/ObjectComparerHasQuery";
+export {ObjPropValueDiff} from "./lib/util/objPropValueDiff";
+export {evalPropValueDiffs} from "./lib/util/evalPropValueDiffs";
+export {ObjectCompare} from "./lib/objectCompare";
