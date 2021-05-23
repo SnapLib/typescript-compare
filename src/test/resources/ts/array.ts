314e00aa117337f378b07ad7bfc1f96d5af580ae
Snap <XxSnapperGeexX@gmail.com>
2021-05-23 02:27:54 -0700

    Init with all mock arrays

---
+++ src/test/resources/ts/array.ts
@@ -0,0 +1,7 @@
+export const ofStrA: unknown[] = ["first", "second", "third"];
+
+export const ofStrB: unknown[] = ["foo", "second", "baz"];
+
+export const ofIntA: unknown[] = [111, 222, 333];
+
+export const ofIntB: unknown[] = [111, 222, 999];
