5a13a79d8be113f93638c967c7745dbefdaffc2d
Snap <XxSnapperGeexX@gmail.com>
2021-05-23 00:06:53 -0700

    Update packages

--- src/test/report.mocharc.cjs
+++ src/test/report.mocharc.cjs
@@ -1,14 +1,16 @@
 "use strict";
 
+const join = require("path").join;
+
 module.exports =
 {
-    ...require("./.mocharc"),
+    ...require(".mocharc"),
     reporter: "mochawesome",
     reporterOptions: [
         "autoOpen=true",
         "consoleReporter=none",
         "json=false",
-        "reportDir=./build/test-report",
+        `reportDir=${join(__dirname, "../../", "build", "test-report")}`,
         "reportFilename=index",
         "reportPageTitle=ObjectCompare test report",
         "reportTitle=SnapLib Object Comparer Test Report",
