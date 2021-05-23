63705fa4687bdf3ff6e0bc073173e28661b65a46
Snap <XxSnapperGeexX@gmail.com>
2021-05-22 18:37:07 -0700

    Remove re-exports pf ObjectCompareCountQuery, ObjectCompareHasQuery, and ObjPropValueDiff

--- src/main/ts/index.ts
+++ src/main/ts/index.ts
@@ -1,5 +1,2 @@
-export {ObjectCompareCountQuery} from "./lib/objComparerQueries/objectCompareCountQuery";
-export {ObjectCompareHasQuery} from "./lib/objComparerQueries/objectCompareHasQuery";
-export {ObjPropValueDiff} from "./lib/util/objPropValueDiff";
 export {evalPropValueDiffs} from "./lib/util/evalPropValueDiffs";
 export {ObjectCompare} from "./lib/objectCompare";
