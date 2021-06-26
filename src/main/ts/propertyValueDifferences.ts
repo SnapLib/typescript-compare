05d269c7cd7786b18b527d4713c5b4918efa4c54
Snap <XxSnapperGeexX@gmail.com>
2021-06-25 21:40:40 -0700

    Reexport getPropertyValueDifferences() function and export PropertyValueDifferences type as default

--- src/main/ts/propertyValueDifferences.ts
+++ src/main/ts/propertyValueDifferences.ts
@@ -1,48 +1,9 @@
-import {PropertyValueDifference} from "./propertyValueDifferences/propertyValueDifference";
-import {isEqual} from "./util/isEqual";
-
 export interface PropertyValueDifferences
 {
     readonly [propertyKey: string]: { readonly sourceValue: Readonly<unknown>,
                                       readonly targetValue: Readonly<unknown> };
 }
 
-/**
- * Consumes 2 non-nullable objects, referred to as the *source* and *target*
- * object, and returns the properties that are present in both, but "altered" in
- * the target object. By altered, that means the property key is the same in
- * both the source and target objects, but it's mapped to a different value in
- * the target object than the source object.
- *
- * @param {NonNullable<Object>} source The object being compared to the passed target object
- * @param {NonNullable<Object>} target The object being compared to the passed source object
- *
- * @returns {PropertyValueDifference[]} An array containing the properties that have
- *          the same keys but differing values between the source and target
- *          objects.
- */
-export const getPropertyValueDifferences = (source: NonNullable<unknown>, target: NonNullable<unknown>): Array<PropertyValueDifference> =>
-{
-    if (typeof source !== "string" && typeof source !== "object" || source === null)
-    {
-        throw new Error( ! source ? `${source} source object argument` : "source object argument not parsable to object");
-    }
+export {getPropertyValueDifferences} from "./propertyValueDifferences/getPropertyValueDifferences";
 
-    if (typeof target !== "string" && typeof target !== "object" || target === null)
-    {
-        throw new Error( ! target ? `${target} target object argument` : "target object argument not parsable to object");
-    }
-
-    const targetObjEntries: readonly Readonly<[string, Readonly<unknown>]>[] =
-        Object.freeze(Object.entries(target));
-
-    return Object.entries(source)
-        .filter(srcObjEntry =>
-            targetObjEntries.some(targetObjEntry =>
-                srcObjEntry[0] === targetObjEntry[0]
-                && ! isEqual(srcObjEntry[1], targetObjEntry[1])))
-        .map(srcObjEntry => (
-            new PropertyValueDifference(srcObjEntry[0],
-                                   srcObjEntry[1],
-                                   targetObjEntries.find(targetObjEntry => srcObjEntry[0] === targetObjEntry[0])?.[1])));
-};
+export type {PropertyValueDifferences as default};
